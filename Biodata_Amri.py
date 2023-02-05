0b4180d4a1e27a37b68aa6a2e5b6039ad9acade7
Mochamad Amri Putra Karyadi <120259701+Amri221@users.noreply.github.com>
2023-02-05 22:45:48 +0700

    Biodata_Amri.py

--- Biodata_Amri.py
+++ Biodata_Amri.py
@@ -6,10 +6,8 @@ nama = "Mochamad Amri Putra Karyadi"
 hobi = "Mengetik dan Bermain badminton"
 alamat = "Gang Selagedang 3"
 kota = "Cianjur"
-makana_kesukaan = "indomie"
 
 print("Nama Lengkap :",nama)
 print("Alamat       :",alamat,"\n               kota ",kota)
 print("Hobi         :",hobi)
-print("Makanan Fav  :",makana_kesukaan)
 print(40*"=")

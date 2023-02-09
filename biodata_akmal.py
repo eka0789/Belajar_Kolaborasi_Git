c63f25f51370fe39ba8261f4f32104d2869baa73
Akaml <122186956+MuhammadAkmalFirdausNahwan@users.noreply.github.com>
2023-02-09 06:11:19 -0800

    Create biodata_akmal.py

---
+++ biodata_akmal.py
@@ -0,0 +1,14 @@
+print ("======================================")
+print ("Program Biodata ederhana Dengan Python")
+print ("======================================")
+
+nama = "Muhammad Akmal Firdaus Nahwan"
+hobi = "Olahraga"
+alamat = "Jl.Prof.Moch.Yamin Gg Santosa"
+kota = "Cianjur"
+
+print("Nama Lengkap :",nama)
+print("Alamat       :",alamat, "\n               Kota",kota)
+print("Hobi         :",hobi)
+
+print ("======================================")

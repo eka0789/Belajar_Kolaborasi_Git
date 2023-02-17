184e99f95811232a13e68730e13f177eea7ab933
Jamal1804 <muhammadjamaluddin148@gmail.com>
2023-02-18 00:17:53 +0700

    Biodata Jamal

---
+++ biodata_Jamal.py
@@ -0,0 +1,14 @@
+print ("==========================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("==========================================")
+
+nama = "Muhammad Jamaluddin Al-Anshori"
+hobi = "Olahraga bulu tangkis"
+alamat = "Cieundeur, Warungkondang"
+kota = "Cianjur, Jawa Barat"
+
+print("Nama lengkap	:",nama)
+print("Alamat		:",alamat,"\n                  Kota",kota)
+print("Hobi 		:",hobi)
+
+print ("==========================================")

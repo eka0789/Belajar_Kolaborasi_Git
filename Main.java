c95dddb36af3902ee98e6cb67ea20336882544b3
Kay221a <shinkay177@gmail.com>
2023-02-21 02:07:28 +0700

    Biodata.Java

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Biodata {
         System.out.println("======================")
         System.out.println("Program Biodata")
         System.out.println("======================")
-        System.out.println("Nama : Moch. Amri PK")
-        System.out.println("Alamat : Cianjut")
-        System.out.println("Hobi : Menetik")
+        System.out.println("Nama : Moch. Amri Putra Karyadi")
+        System.out.println("Alamat : Cianjur")
+        System.out.println("Hobi : Mengetik")
         System.out.println("Udah Aja")
         System.out.println("======================")
     }

c6a20bab5a8a6a7b86f66d4e799b501a8925220a
Kay221a <shinkay177@gmail.com>
2023-02-21 01:55:42 +0700

    ini tambahan File Java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Biodata {
+
+    public static void main(string[] args) {
+
+        System.out.println("======================")
+        System.out.println("Program Biodata")
+        System.out.println("======================")
+        System.out.println("Nama : Moch. Amri PK")
+        System.out.println("Alamat : Cianjut")
+        System.out.println("Hobi : Menetik")
+        System.out.println("Udah Aja")
+        System.out.println("======================")
+    }
+}

e0631f0affe3b1eb8e4d4de19d318e8ac17f48c1
Akaml <122186956+MuhammadAkmalFirdausNahwan@users.noreply.github.com>
2023-02-23 19:05:45 -0800

    Update biodata_akmal.py

--- biodata_akmal.py
+++ biodata_akmal.py
@@ -6,9 +6,10 @@ nama = "Muhammad Akmal Firdaus Nahwan"
 hobi = "Olahraga"
 alamat = "Jl.Prof.Moch.Yamin Gg Santosa"
 kota = "Cianjur"
+propinsi = "Jawa Barat"
 
 print("Nama Lengkap :",nama)
-print("Alamat       :",alamat, "\n               Kota",kota)
+print("Alamat       :",alamat, "\n               Kota",kota, "Propinsi",propinsi)
 print("Hobi         :",hobi)
 
 print ("======================================")

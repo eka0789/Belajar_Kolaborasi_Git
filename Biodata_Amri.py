31ca26cb7d1acd84670a8d0cea2086ab313c8af0
Mochamad Amri Putra Karyadi <120259701+Amri221@users.noreply.github.com>
2023-03-01 15:49:01 +0700

    Biodata_Amri.py

--- Biodata_Amri.py
+++ Biodata_Amri.py
@@ -6,6 +6,9 @@ nama = "Mochamad Amri Putra Karyadi"
 hobi = "Mengetik dan Bermain badminton"
 alamat = "Gang Selagedang 3"
 kota = "Cianjur"
+mas = input("masukkan makanan kesukaan : ")
+if mas == 'nasgor':
+  print("kamu gay")
 
 print("Nama Lengkap :",nama)
 print("Alamat       :",alamat,"\n               kota ",kota)

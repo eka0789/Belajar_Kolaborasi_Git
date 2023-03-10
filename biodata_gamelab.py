94b3be1e76cf3452ef6b0b2e15a9c4ca55e14708
Jamal <123542846+Jamal1804@users.noreply.github.com>
2023-03-10 07:58:20 +0700

    Update biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -7,7 +7,7 @@ hobi = "Membantu mengoptimalkan \n                skill programming"
 alamat = "Jalan Kalisombo No. 18"
 Kota = "Salatiga"
 Propinsi = "Jawa Tengah"
-print ("Ancrit")
+
 print("Nama Lengkap  :",nama)
 
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)

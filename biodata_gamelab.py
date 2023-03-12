62c9a0bce52d538c81d8e387f995cdde139e7304
Mochamad Amri Putra Karyadi <120259701+Amri221@users.noreply.github.com>
2023-03-13 03:01:44 +0700

    update rabu

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -8,9 +8,6 @@ alamat = "Jalan Kalisombo No. 18"
 Kota = "Salatiga"
 Propinsi = "Jawa Tengah"
 
-print("Nama Lengkap  :",nama)
-
-print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
 
 
 print("Hobi          :", hobi)

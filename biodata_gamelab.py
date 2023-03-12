4a1229a18c5ad321cbb11dcf6c8f475e4200ac31
Mochamad Amri Putra Karyadi <120259701+Amri221@users.noreply.github.com>
2023-03-13 03:03:31 +0700

    update rabu

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -1,16 +1,19 @@
 print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
-print ("=======================================")
- 
-
-alamat = "Jalan Kalisombo No. 18"
-Kota = "Salatiga"
-Propinsi = "Jawa Tengah"
 
 
 
-print("Hobi          :", hobi)
 
-print ("Selamat Datang di",nama )
-print ("=======================================")
- 
+
+
+
+
+
+
+
+
+
+
+
+
+

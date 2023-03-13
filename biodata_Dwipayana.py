4695747c4e84996ad25e24c72d02d44b98e98745
GusDwipa <122143339+GusDwipa@users.noreply.github.com>
2023-03-14 06:28:51 +0700

    Add files via upload

---
+++ biodata_Dwipayana.py
@@ -0,0 +1,20 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Ida Bagus Nyoman Gede Dwipayana"
+hobi = "Mendengarkan lagu"
+alamat = "Br.Dinas Pken, Desa Peken Belayu. Marga"
+Kota = "Tabanan"
+Propinsi = "Bali"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 

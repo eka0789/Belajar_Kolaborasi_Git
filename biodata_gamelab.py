4ddc12f26b33928da46434b9a9ee70262c0df1b6
azura156 <122070273+azura156@users.noreply.github.com>
2023-03-14 18:50:43 +0700

    Update biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -9,7 +9,10 @@ Kota = "Cianjur"
 Propinsi = "Jawa Barat"
 
 print("Nama Lengkap  :",nama)
+
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
 print("Hobi          :", hobi)
 
 print ("Selamat Datang di",nama )

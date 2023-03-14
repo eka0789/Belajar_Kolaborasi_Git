26ee085c18d86cb3b4f48a7c19161f7e0f887e71
Jamal <123542846+Jamal1804@users.noreply.github.com>
2023-03-14 08:39:25 +0700

    biodata_jamal.py

--- biodata_jamal.py
+++ biodata_jamal.py
@@ -1,11 +1,19 @@
 print ("==========================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("==========================================")
-
 nama = "Muhammad Jamaluddin Al Anshori"
 hobi = "Olahraga bulu tangkis"
 alamat = "Cieundeur, Warungkondang"
 kota = "Cianjur, Jawa Barat"
+
+
+
+
+
+
+
+
+
 print("Nama lengkap	:",nama)
 print("Alamat		:",alamat,"\n                  Kota",kota)
 print("Hobi 		:",hobi)

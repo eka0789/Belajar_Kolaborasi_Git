e26e18f2457bdd1cf4511530aa674c82720c91fb
Mochamad Amri Putra Karyadi <120259701+Amri221@users.noreply.github.com>
2023-03-14 08:28:21 +0700

    update rabu

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -2,14 +2,20 @@ print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
  
-nama = "Jamal"
-hobi = "jago dalam \n                skill programming"
-alamat = "Jalan tanah"
-Kota = "cianjur"
-Propinsi = "Jawa barat"
+nama = "Gamelab Indonesia"
+hobi = "Membantu mengoptimalkan \n                skill programming"
+alamat = "Jalan Kalisombo No. 18"
+Kota = "Salatiga"
+Propinsi = "Jawa Tengah"
+
 print("Nama Lengkap  :",nama)
+
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
 print("Hobi          :", hobi)
 
 print ("Selamat Datang di",nama )
 print ("=======================================")
+ 
+

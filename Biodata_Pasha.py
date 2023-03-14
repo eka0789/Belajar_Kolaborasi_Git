819a15968c202d83849441788ca3bac307cf6a29
azura156 <122070273+azura156@users.noreply.github.com>
2023-03-14 14:47:53 +0700

    Update Biodata_Pasha.py

--- Biodata_Pasha.py
+++ Biodata_Pasha.py
@@ -9,7 +9,9 @@ Kota = "Cianjur"
 Propinsi = "Jawa Barat"
 
 print("Nama Lengkap  :",nama)
+
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
 print("Hobi          :", hobi)
 
 print ("Selamat Datang di",nama )

85d24401f2e3d9ba27affa607650980884c58597
azura156 <122070273+azura156@users.noreply.github.com>
2023-03-14 13:30:33 +0700

    Add files via upload

---
+++ Biodata_Pasha.py
@@ -0,0 +1,15 @@
+print ("=========================================")
+print ("Prongram Biodata Sedaerhana Dengan Python")
+print ("=========================================")
+
+nama = "Pasha Muhamad Zildan"
+hobi = "Main Game"
+alamat = "Jalan K.H.Asnawi NO 29"
+Kota = "Cianjur"
+
+print("Nama Lengkap  :",nama)
+print("Alamat        :",alamat)
+print("Kota          :", Kota)
+print("Hobi          :", hobi)
+
+print ("=========================================")

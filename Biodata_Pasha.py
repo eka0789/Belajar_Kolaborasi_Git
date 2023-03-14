8d403de65997dc2c3e6363dcc74fd32230c186bc
azura156 <122070273+azura156@users.noreply.github.com>
2023-03-14 13:35:37 +0700

    Update Biodata_Pasha.py

--- Biodata_Pasha.py
+++ Biodata_Pasha.py
@@ -1,15 +1,17 @@
-print ("=========================================")
-print ("Prongram Biodata Sedaerhana Dengan Python")
-print ("=========================================")
-
-nama = "Pasha Muhamad Zildan"
-hobi = "Main Game"
-alamat = "Jalan K.H.Asnawi NO 29"
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Paasha Muhamad Zildan"
+hobi = "bermain game \n                "
+alamat = "Jalan K.H.Asnawi No. 29"
 Kota = "Cianjur"
+Propinsi = "Jawa Barat"
 
 print("Nama Lengkap  :",nama)
-print("Alamat        :",alamat)
-print("Kota          :", Kota)
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
 print("Hobi          :", hobi)
 
-print ("=========================================")
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 

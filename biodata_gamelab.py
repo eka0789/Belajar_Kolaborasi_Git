162f40b366e516fb495dc1b1842cb856e4f229de
GusDwipa <122143339+GusDwipa@users.noreply.github.com>
2023-04-24 15:50:14 +0800

    Update biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -9,7 +9,7 @@ Kota = "Tabanan"
 Propinsi = "Bali"
 
 print("Nama Lengkap  :",nama)
-print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+print("Alamat        :",alamat, "\n                Kota ", Kota, Propinsi)
 print("Hobi          :", hobi)
 
 print ("Selamat Datang di",nama )

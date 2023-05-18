54a1749cd9836a0d959a1749f8e67f5177dd9656
RizkyMahatma <121926756+RizkyMahatma@users.noreply.github.com>
2023-05-18 20:34:05 +0700

    biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -9,6 +9,7 @@ alamat = "Jalan Kalisombo No. 18"
 Kota = "Salatiga"
 Propinsi = "Jawa Tengah"
 
+
 print("Nama Lengkap  :",nama)
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
 print("Hobi          :", hobi)

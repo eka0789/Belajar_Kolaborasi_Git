3dee2a1dbf01c7b5a4d5d0e419a91a797e416aad
RizkyMahatma <121926756+RizkyMahatma@users.noreply.github.com>
2023-05-18 19:50:03 +0700

    biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -10,6 +10,7 @@ Kota = "Salatiga"
 Propinsi = "Jawa Tengah"
 
 print("Nama Lengkap  :",nama)
-print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",
+      Propinsi)
 print("Hobi          :", hobi)
 print ("=======================================")

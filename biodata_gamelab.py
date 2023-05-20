752231716cc6dd5bffb1e2eff440d04dab5925fa
Jamal <123542846+Jamal1804@users.noreply.github.com>
2023-05-20 13:18:52 +0700

    biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -12,9 +12,7 @@ print("Nama Lengkap  :",nama)
 
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
 
-
 print("Hobi          :", hobi)
-
 print ("Selamat Datang di",nama )
 print ("=======================================")
  

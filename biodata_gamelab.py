cdafb57f3ab9c960dddf4f4a50d8d0efa2eca2c2
Jamal <123542846+Jamal1804@users.noreply.github.com>
2023-05-20 13:39:26 +0700

    biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -11,10 +11,7 @@ Propinsi = "Jawa Tengah"
 print("Nama Lengkap  :",nama)
 
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
-
-
 print("Hobi          :", hobi)
-
 print ("Selamat Datang di",nama )
 print ("=======================================")
  

b1009bc15ba299bf5fdd27bdda9785bfe648094a
Rehannx <raehanrp9@gmail.com>
2023-06-20 08:45:00 +0700

    tambah file python

---
+++ biodata_gamelab.py
@@ -0,0 +1,20 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Gamelab Indonesia"
+hobi = "Membantu mengoptimalkan \n                skill programming"
+alamat = "Jalan Kalisombo No. 18"
+Kota = "Salatiga"
+Propinsi = "Jawa Tengah"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 

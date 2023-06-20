06f1e871d9f16451e59ac8e12c2681219bedbf9c
FadilIsk06 <125333398+FadilIsk06@users.noreply.github.com>
2023-06-20 21:58:15 +0700

    biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -1,13 +1,11 @@
 print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
- 
 nama = "Gamelab Indonesia"
 hobi = "Membantu mengoptimalkan \n                skill programming"
 alamat = "Jalan Kalisombo No. 18"
 Kota = "Salatiga"
 Propinsi = "Jawa Tengah"
-
 print("Nama Lengkap  :",nama)
 
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
@@ -17,4 +15,3 @@ print("Hobi          :", hobi)
 
 print ("Selamat Datang di",nama )
 print ("=======================================")
- 

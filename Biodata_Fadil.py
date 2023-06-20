f9dc1a1b26b2e885055957b91042c2ac46b2b122
FadilIsk06 <125333398+FadilIsk06@users.noreply.github.com>
2023-06-20 10:15:29 +0700

    Biodata_Fadil.py

---
+++ Biodata_Fadil.py
@@ -0,0 +1,14 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+
+nama = "Muhammad Fadil Iskandar"
+hobi = "Menyanyi"
+alamat = "Kalibunder Regency Blok J13"
+Kota = "Cianjur"
+
+print("Nama Lengkap	:",nama)
+print("Alamat		:",alamat, "\n		Kota ", Kota)
+print("Hobi		:", hobi)
+
+print ("=======================================")

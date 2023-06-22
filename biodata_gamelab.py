e989ff59de1f2f867e21b06812606baf683eca58
Edojuliansyah <136568933+Edojuliansyah@users.noreply.github.com>
2023-06-22 14:23:30 +0700

    biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -7,14 +7,9 @@ hobi = "Membantu mengoptimalkan \n                skill programming"
 alamat = "Jalan Kalisombo No. 18"
 Kota = "Salatiga"
 Propinsi = "Jawa Tengah"
-
 print("Nama Lengkap  :",nama)
-
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
-
-
 print("Hobi          :", hobi)
-
 print ("Selamat Datang di",nama )
 print ("=======================================")
  

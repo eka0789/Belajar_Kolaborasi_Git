c7583b27641988d831ca9392f69ef45ba6cae4f1
gideonnatha <123616740+gideonnatha@users.noreply.github.com>
2023-06-22 10:39:31 +0700

    Add files via upload

---
+++ Biodata_Gideon.py.py
@@ -0,0 +1,17 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+
+
+
+Nama = "Isi dengan nama kamu"
+hobi = "Isi dengan hobi kamu"
+alamat = "Isi dengan alamat kamu"
+Kota = "Isi dengan kota kamu"
+
+
+
+print("Gideon Nathanael Sihaloho :",nama)
+print("Kp.dangdeur               :"Kp.dangdeur, "\n         Cianjur",kota)
+
+print ("========================================")|

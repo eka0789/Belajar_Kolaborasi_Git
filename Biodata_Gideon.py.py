584e8a3beb40cbbdb02bd3eb1945b6b40c2d1743
gideonnatha <123616740+gideonnatha@users.noreply.github.com>
2023-06-22 11:05:18 +0700

    Update Biodata_Gideon.py.py

--- Biodata_Gideon.py.py
+++ Biodata_Gideon.py.py
@@ -1,15 +1,10 @@
 print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
-print ("=======================================")
-
-
-
+print ("======================================="
 Nama = "Isi dengan nama kamu"
 hobi = "Isi dengan hobi kamu"
 alamat = "Isi dengan alamat kamu"
 Kota = "Isi dengan kota kamu"
-
-
 
 print("Gideon Nathanael Sihaloho :",nama)
 print("Kp.dangdeur               :"Kp.dangdeur, "\n         Cianjur",kota)

d0981a53d4c8c375bdeac50d4f1ddf0d9b6524ce
Edojuliansyah <136568933+Edojuliansyah@users.noreply.github.com>
2023-06-22 14:15:20 +0700

    biodata _edo.py

--- biodata _edo.py
+++ biodata _edo.py
@@ -6,7 +6,6 @@ nama = "Edo juliansyah"
 hobi = "Bermain bola"
 alamat = "Kp. Tipar Des. Ciwalen"
 kota = "Kota Cianjur"
-
 print("Edo juliansyah :",nama)
 print("Kp. Tipar Des. Ciwalen:",alamat, "\n         Cianjur",kota)
 

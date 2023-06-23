81f1fc01a8b55aa73fc6062bc6c6cff18fd17d36
Amess99 <137264479+Amess99@users.noreply.github.com>
2023-06-23 10:20:21 +0700

    Add files via upload

---
+++ biodata_dimas.py
@@ -0,0 +1,12 @@
+print(40*"=")
+print("Program Biodata Sederhana Dengan Python")
+print(40*"=")
+
+nama = "Dimas Permana"
+hobi = "travel"
+alamat = "Cianjur. kec.karangtengah kp.kebonsalak"
+kota = "Cianjur"
+print("Nama Lengkap :",nama)
+print("Alamat       :",alamat,"\n           kota ",kota)
+print("Hobi         :",hobi)
+print(40*"=")

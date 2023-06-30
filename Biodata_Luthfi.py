8a6fa85da2ef2823af45916ed42de4ea4a1d30eb
sanjireca <127289954+sanjireca@users.noreply.github.com>
2023-06-30 15:08:19 +0700

    Add files via upload

---
+++ Biodata_Luthfi.py
@@ -0,0 +1,19 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Muhammad Luthfi Wibiksana"
+hobi = "Nonton Film"
+alamat = "Gunteung"
+Kota = "Cianjur"
+Propinsi = "Jawa Barat"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+print ("=======================================")
+ 
+

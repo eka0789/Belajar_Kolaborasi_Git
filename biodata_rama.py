9d6b3bb986772aa27a41dfdbb67924566b8b2d05
gustirama123 <135805644+gustirama123@users.noreply.github.com>
2023-07-30 00:02:44 +0800

    Add files via upload

---
+++ biodata_rama.py
@@ -0,0 +1,20 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Gusti rama"
+hobi = "Bermain Sepak bola"
+alamat = "Br. Dinas Pesagi"
+Kota = "Tabanan"
+Propinsi = "bali"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 

2adf9bf0f6acdf8447082ad736ac503222329f8d
sitizulaika2013 <140186527+sitizulaika2013@users.noreply.github.com>
2023-08-22 21:29:09 +0700

    Add files via upload

---
+++ Biodata_SitiZulaika.py
@@ -0,0 +1,16 @@
+print ("========================================================================================================================")
+print ("                                       PROGRAM BIODATA SEDERHANA DENGAN PYTHON                                          ")
+print ("========================================================================================================================")
+
+nama_lengkap = "Siti Zulaika"
+alamat = "sawah panjang jorong kp melayu,nagari sitalang,Kec.ampek nagari, Kab.Agam Sumatra barat"
+hobi = "mencoba hal baru, membaca, dan memasak"
+asal_sekolah = "SMKN 2 LUBUK BASUNG"
+
+print ("Nama Lengkap :", nama_lengkap)
+print ("Alamat       :", alamat)
+print ("Hobby        :", hobi)
+print ("Asal Sekolah :", asal_sekolah)
+print ("========================================================================================================================")
+
+

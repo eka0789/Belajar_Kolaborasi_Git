35c7489dc93f2846c2d4dc93fb2f64681ea07c00
Fajryy <138565996+Fajryy@users.noreply.github.com>
2023-09-06 14:38:32 +0700

    Add files via upload

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -2,11 +2,11 @@ print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
  
-nama = "Gamelab Indonesia"
-hobi = "Membantu mengoptimalkan \n                skill programming"
-alamat = "Jalan Kalisombo No. 18"
-Kota = "Salatiga"
-Propinsi = "Jawa Tengah"
+nama = "Muhammad Fajry"
+hobi = "Programming"
+alamat = "Padang"
+Kota = "Lubuk Basung"
+Propinsi = "Sumatera Barat"
 
 print("Nama Lengkap  :",nama)
 

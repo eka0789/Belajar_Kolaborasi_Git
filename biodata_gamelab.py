16f2b4590787d28440168749f867f51cfe699089
Fajryy <138565996+Fajryy@users.noreply.github.com>
2023-09-07 08:43:42 +0700

    Update biodata_gamelab.py

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -2,11 +2,11 @@ print ("=======================================")
 print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
  
-nama = "Muhammad Fajry"
-hobi = "Programming"
-alamat = "Padang"
-Kota = "Lubuk Basung"
-Propinsi = "Sumatera Barat"
+nama = "Gamelab Indonesia"
+hobi = "Membantu mengoptimalkan \n                skill programming"
+alamat = "Jalan Kalisombo No. 18"
+Kota = "Salatiga"
+Propinsi = "Jawa Tengah"
 
 print("Nama Lengkap  :",nama)
 
@@ -17,5 +17,3 @@ print("Hobi          :", hobi)
 
 print ("Selamat Datang di",nama )
 print ("=======================================")
- 
-

adb7123ec23f224120adb131bf42806063e2a746
Vadlan <vadlanibrahim29@gmail.com>
2023-09-07 08:26:10 +0700

    tambah file python

---
+++ biodata_vadlann.py
@@ -0,0 +1,15 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "M.Vadlan Ibrahim"
+hobi = "Futsal"
+alamat = "jalan siti manggopoh"
+Kota = "Lubuk basung"
+
+print("Nama Lengkap  :",nama)
+print("Alamat        :",alamat)
+print("Hobi          :", hobi)
+print("Kota          :",Kota)
+
+print ("=======================================")

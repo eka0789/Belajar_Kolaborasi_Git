2cce9d050478dadb59b4baa25601719fa0ec16ea
Ahmad Danii <140671278+dannz3030@users.noreply.github.com>
2023-09-20 08:49:00 +0700

    Update biodata_dani.py

--- biodata_dani.py
+++ biodata_dani.py
@@ -1,11 +1,11 @@
 print ("========================================")
 print ("Program Biodata Sederhana Dengan Pythonn")
 print ("========================================")
-
+nama = nama
 nama = "Ahmad Dani"
 hobi = "Programing"
 alamat = "Lubuk Basung"
-
+alamat = "Lubuk Basung"
 print ("Nama Lengkap    :", nama)
 print ("Alamat          :", alamat)
 print ("Hobi            :", hobi)

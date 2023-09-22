264aadf3635586cfe0d659e19f2e598d673e82e7
YUSRIZALDI <139300310+YUSRIZALDI@users.noreply.github.com>
2023-09-22 08:15:50 +0700

    Update Biodata_yusrizaldi.py

--- Biodata_yusrizaldi.py
+++ Biodata_yusrizaldi.py
@@ -3,8 +3,10 @@ print ("Program Biodata Sederhana Dengan Python")
 print ("=======================================")
 
 nama = "Yusrizaldi"
+nama = "Yusrizaldi"
 hobi = "Main sepak bola"
 alamat = "Dusun 1 jorong kubu anau,tiku utara"
+kota = "Padang"
 kota = "Padang"
 
 print("Nama Lengkap  :",nama)

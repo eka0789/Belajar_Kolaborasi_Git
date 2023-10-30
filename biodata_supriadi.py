b2fbe61ca2f36a7c767108eca4d4b82e767a9302
supriadi325 <140154587+supriadi325@users.noreply.github.com>
2023-10-30 08:29:22 +0700

    tambah file python

---
+++ biodata_supriadi.py
@@ -0,0 +1,14 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+
+nama = "Supriadi"
+hobi = "Voly"
+alamat = "Kinali"
+kota = "pasaman"
+
+print("Nama Lengkap :",nama)
+print("Alamat       :",alamat, "\n              kota ", kota)
+print("Hobi         :", hobi)  
+
+print("========================================")

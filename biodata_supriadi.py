e9913dbfa6a6ef5c2b6001efea9c9a26dc1cdf5c
supriadi325 <140154587+supriadi325@users.noreply.github.com>
2023-10-31 12:18:10 +0700

    Update biodata_supriadi.py

--- biodata_supriadi.py
+++ biodata_supriadi.py
@@ -7,14 +7,14 @@ hobi = "main bola Volly"
 alamat = "Kinali"
 kota = "pasaman Barat"
 
-nama = "Supriadi"
-hobi = "main bola Voly"
-alamat = "Kinali"
-kota = "pasaman Barat"
-
-
 print("Nama Lengkap :",nama)
 print("Alamat       :",alamat, "\n              kota ", kota)
 print("Hobi         :", hobi)  
 
 print("========================================")
+print("========================================")
+
+print("========================================")
+
+print("========================================")
+

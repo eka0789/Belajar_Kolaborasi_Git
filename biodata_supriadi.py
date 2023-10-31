d87711a237b92959811443378e4e3a0a27297650
supriadi325 <140154587+supriadi325@users.noreply.github.com>
2023-10-31 11:58:15 +0700

    Fix kesalahan biodata_supriadi.py

--- biodata_supriadi.py
+++ biodata_supriadi.py
@@ -7,6 +7,12 @@ hobi = "main bola Voly"
 alamat = "Kinali"
 kota = "pasaman Barat"
 
+nama = "Supriadi"
+hobi = "main bola Voly"
+alamat = "Kinali"
+kota = "pasaman Barat"
+
+
 print("Nama Lengkap :",nama)
 print("Alamat       :",alamat, "\n              kota ", kota)
 print("Hobi         :", hobi)  

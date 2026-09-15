7084cc8ed688673c8546651c577e0a5bf5be9f2e
rangkiasm <rangkiasm@gmail.com>
2026-09-15 14:43:19 +0700

    adding style on line 18

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -15,7 +15,7 @@ print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Pro
 
 print("Hobi          :", hobi)
 
-print ("Selamat Datang di",nama )
+print ("Selamat Datang di :",nama )
 print ("=======================================")
 print ("=======================================")
  

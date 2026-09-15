a5faaedaf075ab334623021c6f4daee9121b0862
rangkiasm <rangkiasm@gmail.com>
2026-09-15 15:03:09 +0700

    is this conflict ?

--- biodata_gamelab.py
+++ biodata_gamelab.py
@@ -1,5 +1,5 @@
 print ("=======================================")
-print ("Program Biodata Sederhana Dengan Python")
+print ("Program Biodata Sederhana Dengan Python", str(1 + 1 + 1))
 print ("=======================================")
  
 nama = "Gamelab Indonesia"
@@ -13,9 +13,9 @@ print("Nama Lengkap  :",nama)
 print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
 
 
-print("Hobi          :", hobi)
+print("Hobi          :", hobi, "Gaming" + "Learning")
 
-print ("Selamat Datang di :",nama )
+print ("Selamat Datang di :::::",nama )
 print ("=======================================")
 print ("=======================================")
  
